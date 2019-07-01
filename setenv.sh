81feb64e6d949d80722e1c5c35a994d9bd33fc63
Andrew Olney <aolney@memphis.edu>
2019-07-01 14:06:22 -0500

    added logging env var

--- setenv.sh
+++ setenv.sh
@@ -2,3 +2,5 @@
 #source this script; e.g. source setenv.sh, to run tests in Chrome browser with display
 JUPYTER_TEST_BROWSER=live; export JUPYTER_TEST_BROWSER
 echo $JUPYTER_TEST_BROWSER
+SOS_DEBUG=ALL; export SOS_DEBUG
+echo "logging to ~/.sos/sos_debug.log"
